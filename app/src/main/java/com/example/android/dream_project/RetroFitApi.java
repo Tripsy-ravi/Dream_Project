606cfb60d5f818ac11f35ea3d64e52f1f5051876
Ravi Tripathi <tripathiravi207@gmail.com>
2022-01-30 14:35:02 +0530

    "MAny files are updated and many new are added"

---
+++ app/src/main/java/com/example/android/dream_project/RetroFitApi.java
@@ -0,0 +1,11 @@
+package com.example.android.dream_project;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Url;
+
+public interface RetroFitApi {
+    @GET
+    Call<MsgModal> getMessage(@Url String url);
+}
+
